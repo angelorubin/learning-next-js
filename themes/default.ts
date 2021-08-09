516b0ce423615d11005cedefc81c57a04c0f660a
angelorubin <angelorubin@gmail.com>
2021-08-08 23:40:56 -0300

    Configurando o tema default do material-ui

---
+++ themes/default.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@material-ui/core";
+import { orange } from "@material-ui/core/colors";
+
+export const theme = createTheme({
+  palette: {
+    common: {
+      white: "#fff",
+      black: "#000",
+    },
+  },
+});
