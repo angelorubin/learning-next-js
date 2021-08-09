999cf63fc77797ee9f7a173e8ea93abeababa07d
angelorubin <angelorubin@gmail.com>
2021-08-08 22:09:54 -0300

    Criando componente Login

---
+++ pages/login.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+  styled,
+  Button,
+  Grid,
+  Paper
+} from "@material-ui/core"
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const Login = () => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <Item>xs=8</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>xs=4</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>xs=4</Item>
+      </Grid>
+      <Grid item xs={8}>
+        <Item>xs=8</Item>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Login
